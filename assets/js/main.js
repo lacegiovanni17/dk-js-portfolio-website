e128a4512b1024f8b4997f602472d83cd8d53428
Chidike Henry <dk1online25@gmail.com>
2023-04-03 13:26:43 +0100

    added new feat to lg screen css

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,7 +174,14 @@ themeButton.addEventListener("click", () => {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
